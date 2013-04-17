ef9a89312eb8705f6e91fb1397b3dc84536fc491
Andrew Gregory <andrew.gregory.8@gmail.com>
2013-04-17 15:52:05 -0400

    add support for buil-in functions
    Signed-off-by: Andrew Gregory <andrew.gregory.8@gmail.com>

--- lib/PerlDiver/Local.pm
+++ lib/PerlDiver/Local.pm
@@ -7,13 +7,62 @@ use parent 'PerlDiver';
 use File::Slurp;
 use Pod::Find;
 
+sub _builtin_function_pod {
+    my ($self, $function) = @_;
+    my $pod = '';
+    my $file = Pod::Find::pod_where( { -inc => 1 }, 'perlfunc' );
+    my @lines = read_file($file);
+    my ( $in_section, $depth ) = ( 0, 0 );
+    my $target_re = qr/^=item(\s+\b\Q$function\E\b)/;
+    my $target_end_re = qr/^=item(?!\s+\b\Q$function\E\b)/;
+
+    for ( my $lineno = 1; $lineno <= @lines; $lineno++ ) {
+        my $line = $lines[ $lineno - 1 ];
+        if ($in_section) {
+            if ( $line =~ /^=over/ ) {
+                $depth++;
+            }
+            elsif ( $line =~ /^=back/ ) {
+                if ( $depth == 0 ) {
+                    last;
+                }
+                else {
+                    $depth--;
+                }
+            }
+            elsif ( $line =~ $target_end_re and $depth == 0 ) {
+                last;
+            }
+            $pod .= $line;
+        }
+        elsif ( $line =~ $target_re ) {
+            $in_section = 1;
+            $pod .= $line;
+        }
+        if ( $line !~ /^=item/ and $in_section ) {
+            next;
+
+        }
+    }
+    if($pod) {
+        $pod = "=over\n\n$pod\n\n=back";
+    }
+    return $pod;
+}
+
 sub pod {
-    my ( $self, $target ) = @_;
-    my $file
-      = Pod::Find::pod_where(
-        { -inc => 1, -dirs => ['/usr/bin/vendor_perl/'] }, $target )
-      or return;
-    my $pod = read_file($file);
+    my ( $self, $target, $opts ) = @_;
+    my $pod;
+    if ( $opts->{function} ) {
+        $pod = $self->_builtin_function_pod($target);
+    }
+    else {
+        my $file
+          = Pod::Find::pod_where(
+            { -inc => 1, -dirs => ['/usr/bin/vendor_perl/'] }, $target )
+          or return;
+        $pod = read_file($file);
+    }
     return $pod;
 }
 
