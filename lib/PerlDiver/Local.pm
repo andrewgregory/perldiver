9c76dc4f6b32daab7c42139da3b31f181f2a69bf
Andrew Gregory <andrew.gregory.8@gmail.com>
2013-04-19 13:50:59 -0400

    add builtin variable support
    Signed-off-by: Andrew Gregory <andrew.gregory.8@gmail.com>

--- lib/PerlDiver/Local.pm
+++ lib/PerlDiver/Local.pm
@@ -7,14 +7,15 @@ use parent 'PerlDiver';
 use File::Slurp;
 use Pod::Find;
 
-sub _builtin_function_pod {
-    my ($self, $function) = @_;
-    my $pod = '';
-    my $file = Pod::Find::pod_where( { -inc => 1 }, 'perlfunc' );
+# Extract a pod section from official documentation
+
+sub _get_pod_section {
+    my ( $self, $file, $section ) = @_;
+    my $pod   = '';
     my @lines = read_file($file);
     my ( $in_section, $depth ) = ( 0, 0 );
-    my $target_re = qr/^=item(\s+\b\Q$function\E\b)/;
-    my $target_end_re = qr/^=item(?!\s+\b\Q$function\E\b)/;
+    my $target_re     = qr/^=item(\s+\Q$section\E)/;
+    my $target_end_re = qr/^=item(?!\s+\Q$section\E)/;
 
     for ( my $lineno = 1; $lineno <= @lines; $lineno++ ) {
         my $line = $lines[ $lineno - 1 ];
@@ -37,17 +38,41 @@ sub _builtin_function_pod {
         }
         elsif ( $line =~ $target_re ) {
             $in_section = 1;
+            # add any previous aliases
+            for ( my $i = $lineno - 1; $i >= 1; $i-- ) {
+                if ( $lines[ $i - 1 ] =~ /^(\s*$|=item\s)/ ) {
+                    $pod = $lines[ $i - 1 ] . $pod;
+                }
+                else {
+                    last;
+                }
+            }
             $pod .= $line;
+            # add any following aliases
+            for ( $lineno++; $lineno <= @lines; $lineno++ ) {
+                if ( $lines[ $lineno - 1 ] =~ /^(\s*$|=item\s)/ ) {
+                    $pod .= $lines[ $lineno - 1 ];
+                }
+                else {
+                    $lineno--;
+                    last;
+                }
+            }
         }
         if ( $line !~ /^=item/ and $in_section ) {
             next;
-
         }
     }
-    if($pod) {
+    if ($pod) {
         $pod = "=over\n\n$pod\n\n=back";
     }
     return $pod;
+}
+
+sub _builtin_function_pod {
+    my ( $self, $function ) = @_;
+    my $file = Pod::Find::pod_where( { -inc => 1 }, 'perlfunc' );
+    return $self->_get_pod_section( $file, $function );
 }
 
 sub pod {
@@ -56,10 +81,14 @@ sub pod {
     if ( $opts->{function} ) {
         $pod = $self->_builtin_function_pod($target);
     }
+    elsif ( $opts->{variable} ) {
+        my $file = Pod::Find::pod_where( { -inc => 1 }, 'perlvar' );
+        $pod = $self->_get_pod_section($file, $target);
+    }
     else {
         my $file
           = Pod::Find::pod_where(
-            { -inc => 1, -dirs => ['.', '/usr/bin/vendor_perl/'] }, $target )
+            { -inc => 1, -dirs => [ '.', '/usr/bin/vendor_perl/' ] }, $target )
           or return;
         $pod = read_file($file);
     }
