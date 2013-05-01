ece8335d3d1db9d6d5fb9a1077a38d6da913ebff
Andrew Gregory <andrew.gregory.8@gmail.com>
2013-04-30 22:41:14 -0400

    MetaCPAN: set default url in module
    Signed-off-by: Andrew Gregory <andrew.gregory.8@gmail.com>

--- lib/PerlDiver/MetaCPAN.pm
+++ lib/PerlDiver/MetaCPAN.pm
@@ -9,12 +9,18 @@ use LWP::Simple;
 
 use URI::Escape qw(uri_escape);
 
+my $default_url = 'http://api.metacpan.org/v0';
+
+sub url {
+    my ($self) = @_;
+    return ($self->{url} or $default_url);
+}
+
 sub pod {
     my ( $self, $target ) = @_;
     my $query_url = sprintf( '%s/pod/%s?content-type=text/x-pod',
-        $self->{url}, uri_escape($target) );
-    my $pod = get($query_url) or return;
-    return $pod;
+        $self->url, uri_escape($target) );
+    return get($query_url);
 }
 
 sub search {
@@ -22,7 +28,7 @@ sub search {
     my $query = uri_escape( join( ' ', map {qq("$_")} @targets ) );
     my $query_url
       = sprintf( '%s/release/_search?size=10&q=status:latest AND %s',
-        $self->{url}, $query );
+        $self->url, $query );
     my $response = get($query_url);
     my @results  = map {
         $_->{_source}->{distribution} =~ s/-/::/g;
@@ -34,7 +40,7 @@ sub search {
 sub source {
     my ($self, $target) = @_;
     my $query_url = sprintf( '%s/source/%s',
-        $self->{url}, uri_escape($target) );
+        $self->url, uri_escape($target) );
     my $source = get($query_url) or return;
     return ($source, 'pl');
 }
